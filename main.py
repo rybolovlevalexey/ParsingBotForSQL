25a0ad2e725a277e18d0ca41da646c54b37087c6
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-11-08 19:35:59 +0300

    реализовано получение документа из тг и его обработка через pandas

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import openpyxl
 import io
 import telebot
 import peewee as pw
@@ -17,38 +16,18 @@ def start_message(message: telebot.types.Message):
 @bot.message_handler(content_types=['document'])
 def handle_document(message):
     file_info = bot.get_file(message.document.file_id)
+    print("Получена информация о новом файле")
     file_path = file_info.file_path
     downloaded_file = bot.download_file(file_path)
-
-    # Сохранение файла на диск
-    file = open("bin_file", "wb")
-    file.write(downloaded_file)
-
-    func1()
-
-
-def func(file_name: str):
-    f = open("bin_file", "rb")
-    binary_data = f.read()
-    bytes_io = io.BytesIO(binary_data)
-
-    workbook = openpyxl.load_workbook(bytes_io)
-    workbook.save(file_name)
-
-
-def func1():
-    # Открытие бинарного файла и чтение его содержимого
-    with open("bin_file", 'rb') as f:
-        binary_data = f.read()
-
-    # Преобразование бинарных данных в объект BytesIO
-    bytes_io = io.BytesIO(binary_data)
-
-    # Чтение данных из Excel файла в DataFrame
-    df = pd.read_excel(bytes_io)
-
-    # Сохранение DataFrame в Excel файл
-    df.to_excel("excel_file.xlsb", index=False)
+    print("Файл скачан в бинарном виде с серверов telegram")
+    df = pd.read_excel(downloaded_file)
+    for index, row in df.iterrows():
+        article = row["PART_NO"]
+        name = row["PART_NAME_RUS"]
+        purchase_price = row["D_ORDER_DNP"]
+        retail_price = row["LIST_PRICE"]
+        print(article, name, purchase_price, retail_price, sep="; ")
+        break
 
 
 if __name__ == "__main__":
--- parsing files.py
+++ parsing files.py
@@ -1,13 +1,11 @@
 import pandas as pd
 from sqlalchemy import create_engine
-
-engine = create_engine('mssql+pyodbc:///?odbc_connect=%s' % (
-    'Driver={Microsoft Access Driver (*.accdb)};'
-    'Dbq=C:/TimetableParserBot_project/files with costs/Dealers price-list OEM 01.01.2023.accdb;'))
-
-df = pd.read_sql_table("files with costs/Dealers price-list OEM 01.01.2023.accdb", engine)
-
-engine = create_engine("mssql+pyodbc:///?odbc_connect='Driver={Microsoft Access Driver (*.accdb)};Dbq={your_db_path};Uid={your_user_name};Pwd={your_password}'")
-df = pd.read_sql_query("SELECT * FROM my_table", engine)
-print(df.head())
-print(df.tail())
+# Артикул, Брэнд, Наименование, Цена закупа, Цена розничная, РРЦ, Дата (загрузки цен)
+df = pd.read_excel("files with costs/DEALER_PRICE_LIST.xlsb")
+for index, row in df.iterrows():
+    article = row["PART_NO"]
+    name = row["PART_NAME_RUS"]
+    purchase_price = row["D_ORDER_DNP"]
+    retail_price = row["LIST_PRICE"]
+    print(article, name, purchase_price, retail_price, sep="; ")
+    break
