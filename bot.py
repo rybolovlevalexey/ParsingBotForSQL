ba9a5ec24def09e0c4c1a095f2215fa3d00bb53b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-11-13 10:44:44 +0300

    сделана обработка поступающих excel и access файлов разных расширений: скачиваются корректно, если размер меньше 50 мб

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import telebot
 import peewee as pw
 import pandas as pd
+import pyodbc
+import tempfile
 
 
 db = pw.SqliteDatabase("db.sqlite3")
@@ -15,6 +17,50 @@ def start_message(message: telebot.types.Message):
 @bot.message_handler(commands=["add_new_brand"])
 def add_new_brand(message: telebot.types.Message):
     bot.send_message(message.chat.id, "Введите название автомобильной марки")
+
+
+@bot.message_handler(content_types=['document'])
+def handle_document(message):
+    file_info = bot.get_file(message.document.file_id)
+    print("Получена информация о новом файле")
+    file_path = file_info.file_path
+    downloaded_file = bot.download_file(file_path)
+    print("Файл скачан в бинарном виде с серверов telegram")
+    if file_path.endswith(".xlsb") or file_path.endswith(".xlsx") or file_path.endswith(".xls"):
+        # excel file
+        df = pd.read_excel(downloaded_file)
+        print("Файл обработан успешно")
+        for index, row in df.iterrows():
+            article = row["PART_NO"].strip()
+            name = row["PART_NAME_RUS"].strip()
+            purchase_price = row["D_ORDER_DNP"].strip()
+            retail_price = row["LIST_PRICE"].strip()
+            print(article, name, purchase_price, retail_price, sep="; ")
+            break
+    elif file_path.endswith(".mdb") or file_path.endswith(".accdb"):
+        # access file
+        suf = ".mdb" if file_path.endswith(".mdb") else ".accdb"
+        temp_file = tempfile.NamedTemporaryFile(suffix=suf, delete=False)
+        temp_file.write(downloaded_file)
+        temp_file.close()
+
+        conn_str = (
+                r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+                r'DBQ=' + temp_file.name + ';'
+        )
+        conn = pyodbc.connect(conn_str)
+        cursor = conn.cursor()
+        tables = [table_info.table_name for table_info in cursor.tables(tableType='TABLE')]
+        if len(tables) == 1:
+            query = 'SELECT * FROM ' + tables[0]
+            df = pd.read_sql(query, conn)
+            print("ok")
+            conn.close()
+            temp_file.close()
+        else:
+            print("Передан файл с некорректным количеством таблиц")
+    else:
+        print("Прислан файл неверного расширения")
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-import io
+import pandas as pd
+import pyodbc
+import tempfile
 
+file_path = "files with costs/Dealers price-list OEM 01.01.2023.accdb"
+with open(file_path, 'rb') as f:
+    downloaded_file = f.read()
 
-@bot.message_handler(content_types=['document'])
-def handle_document(message):
-    file_info = bot.get_file(message.document.file_id)
-    print("Получена информация о новом файле")
-    file_path = file_info.file_path
-    downloaded_file = bot.download_file(file_path)
-    print("Файл скачан в бинарном виде с серверов telegram")
-    df = pd.read_excel(downloaded_file)
-    for index, row in df.iterrows():
-        article = row["PART_NO"]
-        name = row["PART_NAME_RUS"]
-        purchase_price = row["D_ORDER_DNP"]
-        retail_price = row["LIST_PRICE"]
-        print(article, name, purchase_price, retail_price, sep="; ")
-        break
-
+suf = ".mdb" if file_path.endswith(".mdb") else ".accdb"
+temp_file = tempfile.NamedTemporaryFile(suffix=suf, delete=False)
+temp_file.write(downloaded_file)
+temp_file.close()
+print("Создан временный файл")
+conn_str = (
+        r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=' + temp_file.name + ';'
+)
+conn = pyodbc.connect(conn_str)
+cursor = conn.cursor()
+tables = [table_info.table_name for table_info in cursor.tables(tableType='TABLE')]
+if len(tables) == 1:
+    print(tables[0])
+    query = 'SELECT * FROM ' + tables[0]
+    print("here")
+    df = pd.read_sql(query, conn)
+    print("ok")
+    conn.close()
+    temp_file.close()
